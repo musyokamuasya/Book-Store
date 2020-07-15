82fe8f747b29b9737bb5c569fcf27744347667a6
Musyoka Muasya <josephmuasya8@gmail.com>
2020-07-15 20:39:43 +0300

    Design of the markup

---
+++ app.js
@@ -0,0 +1,8 @@
+// Book constructor to handle book logic
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+// UI constructor to handle UI logic
+function UI() {}
