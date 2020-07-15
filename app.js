b7f5429cf0cfc9fc8a3fa34df3a565f99b3e38c2
Musyoka Muasya <josephmuasya8@gmail.com>
2020-07-15 22:00:12 +0300

    Add book to the list

--- app.js
+++ app.js
@@ -6,3 +6,42 @@ function Book(title, author, isbn) {
 }
 // UI constructor to handle UI logic
 function UI() {}
+
+
+
+// Event Listeners
+document.querySelector('#book-form').addEventListener('submit', function(e) {
+    console.log('Submitted...');
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+
+    //     Instantiate a book object
+    const book = new Book(title, author, isbn);
+
+    //     Add book to the table
+    const ui = new UI();
+    ui.addBook(book);
+    ui.clearFields();
+    e.preventDefault();
+});
+
+// Add book to the list
+UI.prototype.addBook = function(book) {
+    const list = document.getElementById('book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+<td>${book.title}</td>
+<td>${book.author}</td>
+<td>${book.isbn}</td>
+<td><a href = "#" class = "delete">X</a></td>
+`;
+    list.appendChild(row);
+
+};
+// Clear all the fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('isbn').value = '';
+    document.getElementById('author').value = '';
+};
